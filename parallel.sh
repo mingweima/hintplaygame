f415b2a685b2d641cbb4460ce203f3cec9ceab4c
mingweima <mma3@chicagobooth.edu>
2021-10-03 11:34:02 -0500

    various new experiments for analysis

--- parallel.sh
+++ parallel.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=hpp # create a short name for your job
 #SBATCH --partition=broadwl
-#SBATCH --ntasks=105      # number of tasks per node
+#SBATCH --ntasks=200      # number of tasks per node
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4000
 #SBATCH --output=cpu.out
 #SBATCH --error=cpu.err
-#SBATCH --time=10:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --partition=broadwl
 #SBATCH --mail-type=all          # send email on job start, end and fail
 #SBATCH --mail-user=mma3@chicagobooth.edu
@@ -26,4 +26,4 @@ conda activate mlval
 srun="srun --exclusive -N1 -n1"
 parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog runtask.log --resume"
 
-$parallel "$srun ./train.sh {1} {2}" ::: {1..21} ::: Att3 Att1 Att2 FF LSTM
+$parallel "$srun ./train.sh {1} {2}" ::: {1..201} ::: Att3
